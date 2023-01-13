1d4a304f9b8988d5ad0da588f371249b4ea89135
rizkyprawirap <rizkyprawirap@gmail.com>
2023-01-13 21:45:07 +0700

    Setup ESLint, Prettier, Husky

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import express, {Application, Express, Request, Response, NextFunction} from "express";
+import express, { Application, Request, Response, NextFunction } from 'express'
 
-const app:Application = express();
-const port:Number = 4000;
+const app: Application = express()
+const port: Number = 4000
 
-app.use("/", (req:Request, res:Response, next:NextFunction) => {
-    res.status(200).send({message: 'OK'})
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ message: 'OK' })
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  console.log(`Server is running on port ${port}`)
 })
